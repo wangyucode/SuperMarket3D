1f8727a45811fc226544bfa8e46ee8ff7c0fa965
wayne <www_wayne_001@163.com>
2017-06-26 17:11:39 +0800

    Initial

---
+++ Assets/Script/CreateCustom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CreateCustom : MonoBehaviour {
+
+    public GameObject customerPrefab;
+    public GameObject doorOut;
+    public GameObject doorIn;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("createCustomer", 1, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void createCustomer()
+    {
+        GameObject customer = Instantiate(customerPrefab);
+        customer.transform.position = doorIn.transform.position;
+        customer.transform.localRotation = Quaternion.Euler(0, 180, 0); 
+        NavMeshAgent agent = customer.GetComponent<NavMeshAgent>();
+        agent.destination = doorOut.transform.position;
+    }
+    
+}
