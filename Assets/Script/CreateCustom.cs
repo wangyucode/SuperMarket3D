def668cd9176b155579a3d55fa7ddb68577a4773
wayne <www_wayne_001@163.com>
2017-06-28 09:22:31 +0800

    fix jump bug

--- Assets/Script/CreateCustom.cs
+++ Assets/Script/CreateCustom.cs
@@ -9,9 +9,11 @@ public class CreateCustom : MonoBehaviour {
     public GameObject doorOut;
     public GameObject doorIn;
 
+    public float repeatRate=3;
+
     // Use this for initialization
     void Start () {
-        InvokeRepeating("createCustomer", 1, 1);
+        InvokeRepeating("createCustomer", 1, repeatRate);
     }
 	
 	// Update is called once per frame
@@ -22,7 +24,7 @@ public class CreateCustom : MonoBehaviour {
     void createCustomer()
     {
         GameObject customer = Instantiate(customerPrefab);
-        customer.transform.position = doorIn.transform.position;
+        customer.transform.position = doorIn.transform.position-new Vector3(0,.5f,0);
         customer.transform.localRotation = Quaternion.Euler(0, 180, 0); 
         NavMeshAgent agent = customer.GetComponent<NavMeshAgent>();
         agent.destination = doorOut.transform.position;
